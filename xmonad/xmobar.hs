ef572c4368ad29dda03c465ce4f3d482a8689f66
Denis Fadeev <denis@denisfadeev.com>
2012-01-19 18:39:33 +0600

    xmonad: Default xmobar settings.

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -2,8 +2,12 @@ Config
   {
     font = "xft:FreeSans:pixelsize=13",
     bgColor = "grey3",
+    fgColor = "white",
+    position = Top,
+    lowerOnStart = True,
     commands = [ Run Date "%_d.%m.%y, %R" "date" 10,
                  Run StdinReader ],
+    sepChar = "%",
     alignSep = "}{",
     template = "%StdinReader% }{ %date%"
   }
